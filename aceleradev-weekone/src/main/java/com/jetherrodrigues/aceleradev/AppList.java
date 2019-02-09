9ae0940ede464c3593f89e1f066d29cd9e484ea9
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-02-09 12:12:56 -0200

    working with list

---
+++ aceleradev-weekone/src/main/java/com/jetherrodrigues/aceleradev/AppList.java
@@ -0,0 +1,55 @@
+package com.jetherrodrigues.aceleradev;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 
+ * @author jether.rodrigues
+ *
+ */
+public class AppList {
+
+	public static void main(String[] args) {
+		/**
+		 * List of fruits
+		 */
+		List<String> frutas = new ArrayList<>();
+		frutas.add("Banana");
+		frutas.add("Melancia");
+		frutas.add("Melancia");
+		frutas.add("Melao");
+		frutas.add("Morango");
+		
+		System.out.println(frutas.indexOf("Melao"));
+		
+		/**
+		 * Set of products
+		 */
+		Set<String> produtos = new HashSet<>();
+		produtos.add("Caneta");
+		produtos.add("Caneta");
+		produtos.add("Notebook");
+		produtos.add("Mesa");
+		produtos.add("Caneca");
+		
+		/**
+		 * print fruits
+		 */
+		System.out.println(">> Frutas: ");
+		for (String string : frutas) {
+			System.out.println(string);
+		}
+		System.out.println();
+		
+		/**
+		 * print products
+		 */
+		System.out.println(">> Produtos: ");
+		for (String string : produtos) {
+			System.out.println(string);
+		}
+	}
+}
